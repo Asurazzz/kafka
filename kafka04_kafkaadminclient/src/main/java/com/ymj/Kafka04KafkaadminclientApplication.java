23f14c09c2595b4a6fc09a609fcbed1b3ab651cb
ymj <2513667783@qq.com>
2021-08-24 16:04:16 +0800

    添加kafkaAdminClient

---
+++ kafka04_kafkaadminclient/src/main/java/com/ymj/Kafka04KafkaadminclientApplication.java
@@ -0,0 +1,13 @@
+package com.ymj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka04KafkaadminclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kafka04KafkaadminclientApplication.class, args);
+    }
+
+}
---
+++ kafka04_kafkaadminclient/src/test/java/com/ymj/Kafka04KafkaadminclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka04KafkaadminclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
